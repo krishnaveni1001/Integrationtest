b52f2608352cfef4a052e9edbbff31874681f370
krishnavenics <krishnaveni.valluru@cargosphere.com>
2019-07-24 14:31:54 -0400

    First commit

---
+++ src/test/java/Integration/Integrationtest/RunnerClass.java
@@ -0,0 +1,26 @@
+package Integration.Integrationtest;
+
+
+
+import org.junit.runner.RunWith;
+
+import cucumber.api.CucumberOptions;
+
+import cucumber.api.junit.Cucumber;
+
+@RunWith(Cucumber.class)
+
+@CucumberOptions(
+		plugin = {"pretty" ,"html:target/cucumber-html-report",
+                "json:target/testresult/cucumber.json","pretty:target/cucumber-pretty.txt",
+                "junit:target/cucumber-results.xml"},
+				
+        features= { "src/test/java/demo.feature" },
+        		 format={"pretty","html:target/Reports"}
+		)
+public class RunnerClass {
+	
+	
+	
+
+}
---
+++ src/test/java/Integration/Integrationtest/StepDefinition.java
@@ -0,0 +1,37 @@
+package Integration.Integrationtest;
+
+import cucumber.api.java.en.Given;
+import cucumber.api.java.en.Then;
+
+public class StepDefinition{
+
+@Given("^I login to CargoUI$")
+public void i_login_to_CargoUI() throws Throwable {
+    // Write code here that turns the phrase above into concrete actions
+   
+}
+
+@Given("^I give username and password$")
+public void i_give_username_and_password() throws Throwable {
+    // Write code here that turns the phrase above into concrete actions
+    
+}
+
+@Given("^I click OK button$")
+public void i_click_OK_button() throws Throwable {
+    // Write code here that turns the phrase above into concrete actions
+   
+}
+
+@Given("^I enter to RFQ to verify RFQ number$")
+public void i_enter_to_RFQ_to_verify_RFQ_number() throws Throwable {
+    // Write code here that turns the phrase above into concrete actions
+    
+}
+
+@Then("^the data should match for the entered RFQ$")
+public void the_data_should_match_for_the_entered_RFQ() throws Throwable {
+    // Write code here that turns the phrase above into concrete actions
+   
+}
+}
